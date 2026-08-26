48c5c028668396f3aeb6678554add62ea95c2da8
neotee314 <abheidari314@gmail.com>
2026-08-27 00:57:25 +0200

    update project

---
+++ e-commerce-system/src/main/java/com/neotee/ecommercesystem/shopsystem/storageunit/domain/StorageUnitComparator3.java
@@ -0,0 +1,34 @@
+//package com.neotee.ecommercesystem.shopsystem.storageunit.domain;
+//
+//import com.neotee.ecommercesystem.domainprimitives.ZipCode;
+//import com.neotee.ecommercesystem.shopsystem.product.domain.Product;
+//
+//import java.util.Comparator;
+//import java.util.Map;
+//
+//
+//public class StorageUnitComparator3 implements Comparator<StorageUnit> {
+//    private final Map<Product, Integer> unfulfilledItems;
+//    private final ZipCode clientZipCode;
+//
+//    public StorageUnitComparator3(Map<Product, Integer> unfulfilledItems, ZipCode clientZipCode) {
+//        this.unfulfilledItems = unfulfilledItems;
+//        this.clientZipCode = clientZipCode;
+//    }
+//
+//    @Override
+//    public int compare(StorageUnit su1, StorageUnit su2) {
+//        int contribCompare = Integer.compare(
+//                su2.getTotalContributingItems(unfulfilledItems),
+//                su1.getTotalContributingItems(unfulfilledItems)
+//        );
+//        if (contribCompare != 0) {
+//            return contribCompare;
+//        }
+//        return Integer.compare(
+//                su1.getDistanceToClient(clientZipCode),
+//                su2.getDistanceToClient(clientZipCode)
+//        );
+//    }
+//}
+//
